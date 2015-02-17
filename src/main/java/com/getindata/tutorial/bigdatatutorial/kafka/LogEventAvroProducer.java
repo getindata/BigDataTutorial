58366a9225de80af12fe718bbbf8397e11c71e0c
Adam Kawa <kawa.adam@gmail.com>
2015-02-17 09:59:39 +0100

    add toBytes method

--- src/main/java/com/getindata/tutorial/bigdatatutorial/kafka/LogEventAvroProducer.java
+++ src/main/java/com/getindata/tutorial/bigdatatutorial/kafka/LogEventAvroProducer.java
@@ -1,14 +1,21 @@
 package com.getindata.tutorial.bigdatatutorial.kafka;
 
-import java.util.*;
-
-import com.getindata.tutorial.bigdatatutorial.avro.LogEvent;
-import com.getindata.tutorial.bigdatatutorial.utils.LogEventAvroGenerator;
-import com.linkedin.camus.etl.kafka.coders.KafkaAvroMessageEncoder;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
+
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import com.getindata.tutorial.bigdatatutorial.avro.LogEvent;
+import com.getindata.tutorial.bigdatatutorial.utils.LogEventAvroGenerator;
+import com.linkedin.camus.etl.kafka.coders.KafkaAvroMessageEncoder;
 
 public class LogEventAvroProducer {
 	public static void main(String[] args) {
@@ -17,7 +24,7 @@ public class LogEventAvroProducer {
 
 		props.put("metadata.broker.list", args[0]);
 		props.put("zk.connect", args[1]);
-        props.put("serializer.class", "kafka.serializer.DefaultEncoder");
+		props.put("serializer.class", "kafka.serializer.DefaultEncoder");
 		props.put("producer.type", "sync");
 		props.put("request.required.acks", "1");
 		props.put("topic", args[2]);
@@ -27,22 +34,35 @@ public class LogEventAvroProducer {
 
 		LogEventAvroGenerator generator = new LogEventAvroGenerator();
 
-		KafkaAvroMessageEncoder enc = new KafkaAvroMessageEncoder(
-				props.getProperty("topic"), null);
-
-		enc.init(props, props.getProperty("topic"));
-
 		for (int i = 0; i < 100000; i++) {
 
 			LogEvent logEvent = generator.next();
 
 			KeyedMessage<String, byte[]> data = new KeyedMessage<String, byte[]>(
-					props.getProperty("topic"), generator.getKey(),
-					enc.toBytes(logEvent));
+					props.getProperty("topic"), generator.getKey(), toBytes(logEvent));
 
 			producer.send(data);
 		}
 
 		producer.close();
+	}
+
+	private static final EncoderFactory encoderFactory = EncoderFactory.get();
+
+	public static byte[] toBytes(LogEvent logEvent) {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		BinaryEncoder encoder = encoderFactory.directBinaryEncoder(
+				outputStream, null);
+
+		DatumWriter<LogEvent> userDatumWriter = new SpecificDatumWriter<LogEvent>(
+				LogEvent.class);
+		try {
+			userDatumWriter.write(logEvent, encoder);
+			encoder.flush();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		return outputStream.toByteArray();
 	}
 }
