c758a68e02ac2103988736e5877320c4ddd24ab0
Adam Kawa <kawa.adam@gmail.com>
2015-02-19 14:36:07 +0100

    cleanup

--- src/main/scala/streaming/bin/start.sh
+++ src/main/scala/streaming/bin/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+SPARK_DIR=/root/spark
+
 ${SPARK_DIR}/bin/spark-submit \
-	--jars target/bigdatatutorial-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-	--master local[4]
+	--master local[4] \
 	--class "KafkaRealtimeKPIs" \
-	target/scala-2.10/Sparking-assembly-0.1.jar
+	target/scala-2.10/Sparking-assembly-0.1.jar \
+	"$@"
