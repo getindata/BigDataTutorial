0e0144651fe66fe958233e66179db9333e6d3d33
Adam Kawa <kawa.adam@gmail.com>
2015-11-07 21:49:44 +0100

    Using New Kafka Producer

--- kafka/src/main/java/com/getindata/tutorial/bigdatatutorial/kafka/LogEventTsvProducer.java
+++ kafka/src/main/java/com/getindata/tutorial/bigdatatutorial/kafka/LogEventTsvProducer.java
@@ -1,50 +1,88 @@
 package com.getindata.tutorial.bigdatatutorial.kafka;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
-import kafka.javaapi.producer.Producer;
-import kafka.producer.KeyedMessage;
-import kafka.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import com.getindata.tutorial.bigdatatutorial.utils.LogEventTsvGenerator;
 
 public class LogEventTsvProducer {
-	public static void main(String[] args) throws InterruptedException {
-
-        if (args.length < 3) {
-            System.err.println("Usage: LogEventTsvProducer <kafkaBroker1:port,kafkaBroker2:port> <zkQuorum> <topic>");
-            System.exit(1);
-        }
-
+	
+	private static int NUM_EVENTS = 10000;
+	
+	private static Properties configureProperties(String bootstrapServers) {
+		
 		Properties props = new Properties();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+				StringSerializer.class.getName());
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+				StringSerializer.class.getName());
+		
+		props.put(ProducerConfig.ACKS_CONFIG, "all");
+		
+		return props;
+		
+	}
+	
+	private static Callback getCallback() {
+		Callback callback = new Callback() {
+            public void onCompletion(RecordMetadata metadata, Exception e) {
+                if(e != null)
+                    e.printStackTrace();
+                	System.out.println("The offset of the record we just sent is: " + metadata.offset());
+            };
+       };
+       return callback;
+	}
+	
+	public static void main(String[] args) throws InterruptedException,
+			ExecutionException {
 
-		props.put("metadata.broker.list", args[0]);
-		props.put("zk.connect", args[1]);
-		props.put("serializer.class", "kafka.serializer.DefaultEncoder");
-		props.put("producer.type", "sync");
-		props.put("request.required.acks", "1");
-		props.put("topic", args[2]);
+		if (args.length < 3) {
+			System.err
+					.println("Usage: LogEventTsvProducer <broker1:port,broker2:port> <topic> <is_sync>");
+			System.exit(1);
+		}
+		
+		// set parameters
+String bootstrapServers = args[0];
+String topic = args[1];
+boolean isSync = Boolean.parseBoolean(args[2]);
 
-		ProducerConfig config = new ProducerConfig(props);
-		Producer<String, byte[]> producer = new Producer<String, byte[]>(config);
 
+			 
+		Properties props = configureProperties(bootstrapServers);
+		
+		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+		
 		LogEventTsvGenerator generator = new LogEventTsvGenerator();
-
-		for (int i = 0; i < 10000; i++) {
-
+		
+		for (int i = 0; i < NUM_EVENTS; ++i) {
+			
+			String key = generator.getKey();
 			String logEvent = generator.next();
 
-			KeyedMessage<String, byte[]> data = new KeyedMessage<String, byte[]>(
-					props.getProperty("topic"), generator.getKey(), logEvent.getBytes());
-
-			producer.send(data);
+			ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(
+					topic, key, logEvent);
+			
+			Future<RecordMetadata> future = producer.send(producerRecord, getCallback());
+			if (isSync) {
+				future.get();
+			}
 
 			if (i % 200 == 0) {
-				 Thread.sleep(500);
 				System.out.println("Produced events: " + i);
 			}
 		}
-
 		producer.close();
+
 	}
 }
