31f0f43576a932fb9c04c6c40e28787d109959c9
Adam Kawa <kawa.adam@gmail.com>
2016-02-12 16:42:08 +0100

    better formatting

--- sparkcore/etl.py
+++ sparkcore/etl.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     # Grab the input and output
     input = sys.argv[1]
     output = sys.argv[2]
-    debug = sys.argv[3] if sys.argv[3] is not None else False
+    debug = sys.argv[3] if len(sys.argv) == 4 is not None else False
 
     # Create a context for the job. The context is used to manage the job at a high level.
     appName = "ETL-%s" % (getpass.getuser())
@@ -29,14 +29,16 @@ if __name__ == "__main__":
 
     # Read in the dataset
     logs = sc.textFile(input)
-    records = logs.map(lambda line: line.split('\t'))
-    streams = records.filter(lambda record: record[3] == 'SongPlayed')
+    events = logs.map(lambda line: line.split('\t'))
+    streams = events.filter(lambda record: record[3] == 'SongPlayed')
     streams_projected = streams.map(lambda record: (remove_braces(record[0]), record[1], record[2], record[4], record[5]))
     streams_correct = streams_projected.filter(lambda (timestamp, host, userId, songId, duration): is_duration_correct(duration))
     streams_unique = streams_correct.distinct()
 
+    # Convert to TSV form
+    streams_unique_tsv = streams_unique.map(lambda record: '\t'.join(record))
     # Save the results in the specified output directory.
-    streams_unique.saveAsTextFile(output)
+    streams_unique_tsv.saveAsTextFile(output)
 
     if dump:
         # Dump content for debugging purposes
