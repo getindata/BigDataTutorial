5b8787da839b3f6c1a1e1ab4be40d0251ff87c16
Adam Kawa <kawa.adam@gmail.com>
2016-04-06 17:10:24 +0200

    fixing empty tuple problem

--- storm-starter/multilang/resources/splitsentence.py
+++ storm-starter/multilang/resources/splitsentence.py
@@ -17,8 +17,9 @@ import storm
 
 class SplitSentenceBolt(storm.BasicBolt):
     def process(self, tup):
-        words = tup.values[0].split(" ")
-        for word in words:
-          storm.emit([word])
+        if tup:
+            words = tup.values[0].split(" ")
+            for word in words:
+                storm.emit([word])
 
 SplitSentenceBolt().run()
