cb4e64e98428892086e8dcae619902dd45bd15bd
pkrewski <piotr.krewski@gmail.com>
2017-01-25 17:15:48 +0000

    Add more comments

--- spark/etl_dataframe_jupyter.py
+++ spark/etl_dataframe_jupyter.py
@@ -8,12 +8,12 @@ import sys
 def isDurationCorrect(duration):
     return duration is not None and duration != 'INF' and 30 <= int(duration) and int(duration) <= 1200
 
-def removeBraces(text):
+def removeBrackets(text):
     return text[1:-1]
 
 # Register UDFs
 udfIsDurationCorrect=udf(isDurationCorrect, BooleanType())
-udfRemoveBraces=udf(removeBraces, StringType())
+udfRemoveBrackets=udf(removeBrackets, StringType())
 
 # Define global variables
 database = <your-database>
@@ -22,11 +22,22 @@ database = <your-database>
 logs = sqlContext.sql("SELECT * FROM %s.logs" % database)
 
 # Process the dataset
+# Leave only SongPlayed events
 streams_raw = logs.***TODO***(logs['eventType'] == 'SongPlayed')
+
+# Remove unnecessary 'eventType' column from dataset
 streams_projected = streams_raw.***TODO***('eventType')
+
+# Rename 'itemId' column into 'trackId'
 streams = streams_projected.withColumnRenamed('itemId', 'trackId')
+
+# Exclude records with incorrect value in 'duration' field 
 streams_correct = streams.***TODO***(udfIsDurationCorrect('duration'))
-streams_cleaned = streams_correct.***TODO***(udfRemoveBraces('ts').alias('ts'), 'host', 'userId', 'trackId', streams_correct['duration'].cast('int'))
+
+# Remove brackets around 'ts' field and cast 'duration' field to integer type
+streams_cleaned = streams_correct.***TODO***(udfRemoveBrackets('ts').alias('ts'), 'host', 'userId', 'trackId', streams_correct['duration'].cast('int'))
+
+# Remove duplicates from dataset
 streams_unique = streams_cleaned.***TODO***()
 
 # Store dataset
